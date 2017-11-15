9bf6b148dcb3663bbdea617b7bdbe8cf01ca6df4
Class Account <ee106a-aam@imail.EECS.Berkeley.EDU>
2017-11-14 18:17:07 -0800

    showing one image working

--- src/camera_test_pkg/src/camera_listener_sub.py
+++ src/camera_test_pkg/src/camera_listener_sub.py
@@ -12,6 +12,8 @@ from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
 
+global img, once
+
 def detect_circles(img):
     cv2.medianBlur(img, 5)
     cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
@@ -21,26 +23,42 @@ def detect_circles(img):
 
 
 def callback(data):
-    print("WORKING")
+    global image, once
+
+    if once:
+        cv2.imshow("sample", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        sys.exit()
+
+    once = True
+
     rospy.logdebug("Received image")
+
     try:
-        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+        image = bridge.imgmsg_to_cv2(data, "bgr8")
+        print(type(image))
     except CvBridgeError as e:
         print(e)
 
-    cv2.imshow("sample", cv_image)
+    
 
-    print("Image size: ({0}, {1})".format(cv_image.shape[0], cv_image.shape[1]))
-    return cv_image
+    print("Image size: ({0}, {1})".format(image.shape[0], image.shape[1]))
+    # subscriber.unregister(subscriber)
 
 
 def main():
     # initialize subscriber to take one image #
+    global once
+    once = False
     rospy.init_node("circle_detection")
-    data = rospy.Subscriber("io/internal_cameras/head_camera/image_rect_color", Image, callback)
+    img = rospy.Subscriber("/io/internal_camera/head_camera/image_rect_color", Image, callback)
+    # cv2.imshow("sample", bridge.imgmsg_to_cv2(take_image, "bgr8"))
 
     try:
         rospy.spin()
+        once = True
+        # cv2.imshow("sample", img)
     except KeyboardInterrupt:
         print("Shutting down")
 
